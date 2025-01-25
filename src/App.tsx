416534858bfa7f1d26b4a020e7c477c639f83e46
itayf <itayfi@gmail.com>
2025-01-25 13:04:01 +0200

    Add win message

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import "@/lib/get-best-coloring.ts";
 import { useWordleStore } from "@/lib/store.ts";
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { toast } from "sonner";
+import { cn } from "@/lib/utils.ts";
 
 function App() {
   const addWord = useWordleStore(({ addWord }) => addWord);
@@ -19,55 +20,72 @@ function App() {
   );
 
   return (
-    <div className="max-w-[432px] min-h-dvh flex flex-col mx-auto px-6 pt-12 pb-6 gap-6">
+    <>
       <Toaster />
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-        וורדל מניאק
-      </h1>
-      <div className="grow shrink-0">
-        {(isWin
-          ? guesses
-          : [...guesses, { word: typedWord, coloring: null }]
-        ).map(({ word, coloring }, idx) => (
-          <WordleRow word={word} coloring={coloring} key={idx} />
-        ))}
-      </div>
-      <Keyboard
-        disabled={isWin}
-        onKeyDown={(key) =>
-          setTypedWord((word) => {
-            if (word.length >= 5) {
-              return word;
-            }
-            return `${word}${key}`;
-          })
-        }
-        onBackspace={() => setTypedWord((word) => word.slice(0, -1))}
-        onEnter={() => {
-          if (typedWord.length < 5) {
-            return;
+      <div className="max-w-[432px] min-h-dvh flex flex-col mx-auto px-6 pt-6 pb-6 gap-6">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          וורדל מניאק
+        </h1>
+        <div className={cn("shrink-0", { grow: !isWin })}>
+          {(isWin
+            ? guesses
+            : [...guesses, { word: typedWord, coloring: null }]
+          ).map(({ word, coloring }, idx) => (
+            <WordleRow word={word} coloring={coloring} key={idx} />
+          ))}
+        </div>
+        {isWin ? (
+          <div className="grow">
+            נו, ניצחת תוך {guesses.length} צעדים.
+            {guesses.length < 8
+              ? " זה כנראה היה מזל."
+              : " נקווה שילך יותר טוב בפעם הבאה."}
+          </div>
+        ) : null}
+        <Keyboard
+          disabled={isWin}
+          onKeyDown={(key) =>
+            setTypedWord((word) => {
+              if (word.length >= 5) {
+                return word;
+              }
+              return `${word}${key}`;
+            })
           }
-          addWord(typedWord)
-            .then((success) => {
-              if (success) {
-                setTypedWord("");
-              } else {
-                toast("המילה לא קיימת במילון", {
+          onBackspace={() => setTypedWord((word) => word.slice(0, -1))}
+          onEnter={() => {
+            if (typedWord.length < 5) {
+              return;
+            }
+            addWord(typedWord)
+              .then((success) => {
+                if (success) {
+                  setTypedWord("");
+                } else {
+                  toast("לא להמציא לי כאן מילים", {
+                    position: "top-center",
+                  });
+                }
+              })
+              .catch(() => {
+                toast("הבאג לא אצלי הוא אצלך", {
                   position: "top-center",
                 });
-              }
-            })
-            .catch(() => {
-              toast("אופסי", {
-                position: "top-center",
               });
-            });
-        }}
-      />
-      <div className="text-center">
-        <Button onClick={reset}>משחק חדש</Button>
+          }}
+        />
+        <div className="text-center">
+          <Button
+            onClick={(event) => {
+              reset();
+              event.currentTarget.blur();
+            }}
+          >
+            משחק חדש
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
